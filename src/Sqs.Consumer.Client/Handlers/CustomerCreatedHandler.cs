bb4374bf8e4bcf43c067973d90d500af96d45d27
Cody Skidmore <4048971+codyskidmore@users.noreply.github.com>
2023-03-30 11:38:20 -0400

    Pushing dead letter implementation before continuing to Simple Notification Service (SNS) implementation.

--- src/Sqs.Consumer.Client/Handlers/CustomerCreatedHandler.cs
+++ src/Sqs.Consumer.Client/Handlers/CustomerCreatedHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Options;
 using Sqs.Consumer.Client.Messages;
 
 namespace Sqs.Consumer.Client.Handlers;
@@ -6,14 +7,21 @@ namespace Sqs.Consumer.Client.Handlers;
 public class CustomerCreatedHandler: IRequestHandler<CustomerCreated>
 {
     private readonly ILogger<CustomerCreatedHandler> _logger;
+    private readonly IOptions<QueueOptions> _options;
 
-    public CustomerCreatedHandler(ILogger<CustomerCreatedHandler> logger)
+    public CustomerCreatedHandler(ILogger<CustomerCreatedHandler> logger, IOptions<QueueOptions> options)
     {
         _logger = logger;
+        _options = options;
     }
 
     public Task Handle(CustomerCreated request, CancellationToken cancellationToken)
     {
+        if (_options.Value.AllowDeadLetterTest)
+        {
+            throw new Exception("Dead Letter Test");
+        }
+
         _logger.LogInformation(request.FullName);
         return Unit.Task;
     }
--- src/Sqs.Consumer.Client/QueueOptions.cs
+++ src/Sqs.Consumer.Client/QueueOptions.cs
@@ -3,6 +3,7 @@
 public class QueueOptions
 {
     public required string QueueName { get; init; }
-    public required int WaitPeriod { get; set; }
-    public required string MessageTypeNamespace { get; set; }
+    public required int WaitPeriod { get; init; }
+    public required string MessageTypeNamespace { get; init; }
+    public required bool AllowDeadLetterTest { get; init; }
 }
